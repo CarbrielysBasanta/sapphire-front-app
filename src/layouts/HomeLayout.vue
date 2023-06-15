a7395ee00ba7d0670ff3d87e8235cf9e8d9904a6
CarbrielysBasantaG <orinocoDevTeDe6@gmail.com>
2023-06-15 14:00:04 -0400

    Dashboard

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,55 +1,54 @@
 <template>
-  <q-layout view="hHh lpR fFf">
+  <q-layout view="hHh lpR fFf" style="background-color: #ECECEC;">
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" width="110" side="left" overlay
+    <q-drawer show-if-above v-model="leftDrawerOpen" width="110" side="left"
       style="background: linear-gradient(to bottom, #8D094D, #1441B5); border-radius: 0px 40px 40px 0px;"
       class="row justify-center">
-      <div class="col-md-12">
+      <div class="col-md-12" style="height: 22%;">
         <q-img src="../img/SapphireLogo.png" class="q-my-xs" style="height: 70px; width: 100%" />
       </div>
-      <div>
+      <div style="height: 78%;">
         <q-list>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="q-mb-sm">
             <q-item-section avatar>
-              <q-btn flat to="/" icon="mdi-home" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
+              <q-btn flat to="/" size="20px" icon="mdi-home" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
               </q-btn>
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="q-mb-sm">
             <q-item-section avatar>
-              <q-btn flat to="/" icon="mdi-rhombus-outline" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
+              <q-btn flat to="/" size="20px" icon="mdi-rhombus-outline"
+                :color="$route.name === 'dashboard' ? 'white' : 'accent'">
               </q-btn>
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="q-mb-sm">
             <q-item-section avatar>
-              <q-btn flat to="/" icon="mdi-earth" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
+              <q-btn flat to="/" size="20px" icon="mdi-earth" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
               </q-btn>
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="q-mb-sm">
             <q-item-section avatar>
-              <q-btn flat to="/" icon="mdi-account-group" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
+              <q-btn flat to="/" size="20px" icon="mdi-account-group"
+                :color="$route.name === 'dashboard' ? 'white' : 'accent'">
               </q-btn>
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple style="position: fixed; bottom: 0; margin-left: 7.5px">
+          <q-item clickable v-ripple style="position: fixed; bottom: 0;">
             <q-item-section avatar>
-              <q-btn flat to="/" icon="mdi-logout" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
+              <q-btn flat to="/" size="20px" icon="mdi-logout" :color="$route.name === 'dashboard' ? 'white' : 'accent'">
               </q-btn>
             </q-item-section>
           </q-item>
-          <!-- <q-item clickable v-ripple>
-            <q-item-section class="row justify-center items-center" avatar>
-              <q-icon class="col-12" color="white" name="bluetooth" />
-            </q-item-section>
-          </q-item> -->
         </q-list>
       </div>
     </q-drawer>
 
-    <div class=" col-12 row" style="height: 100vh; width: 100vw; background-color: #ECECEC;">
-      <q-page-container class=" row no-padding no-margin" style=" overflow: hidden;">
+
+
+    <div class=" col-12" style="height: 700px; width: 1480px; background-color: #ECECEC;">
+      <q-page-container class="row">
         <router-view></router-view>
       </q-page-container>
     </div>
@@ -76,8 +75,9 @@ export default {
 </script>
 
 <style>
-.q-drawer--on-top {
+.q-drawer--standard {
   background-color: #ECECEC;
+  /* opacity: 0.1; */
 }
 
 .q-item__section--side {
--- src/pages/HomeMenuPage.vue
+++ src/pages/HomeMenuPage.vue
@@ -1,27 +1,191 @@
 
 <template>
-  <div class=" row col-12 absolute" style=" height: 100vh; ">
+  <div class=" row col-12" style=" height: 100vh;">
+    <div class="row justify-end items-center col-9">
+      <div class="col-10 bg-accent q-mr-xl row justify-center" style="height: 85%; border-radius: 0px 100px 0px 100px;">
+        <div class="col-12 row justify-between" style="height: 24%;">
+          <div class="bg-white q-ml-lg q-mt-lg col-7 row justify-center items-center content-center"
+            style="border-radius: 0px 60px 0px 60px; height: 60%;">
+            <div class="col-12 row justify-start items-center">
+              <p class=" col-10 q-ml-md text-weight-bold text-h5 no-marging">¡Hola, {{ userName }}!</p>
+            </div>
+            <div class="col-12 row justify-end">
+              <p class="text-italic text-weight-light text-h6 q-mr-md">Bienvenido al baúl de los mundos fantásticos.</p>
+            </div>
+          </div>
+          <div class="col-3 q-mr-xl q-mt-lg" style="height: 100px;">
+            <q-img src="../img/Sapphire.png" class="q-my-xs" style="height: 100px; width: 100%" />
+          </div>
+        </div>
+        <div class="col-12 row justify-center items-start" style="height: 38%;">
+          <div class="col-11 row">
+            <div class="col-12 row justify-start items-center ">
+              <p class="col-10 q-mb-md text-weight-medium text-h4 no-marging">Lugares</p>
+            </div>
 
+            <div class="col-12 bg-primary row justify-center items-center" style="height: 150px; border-radius: 38px;">
+              <div class="col-12 row" :class="array == 2 ? 'justify-start' : 'justify-between'" style="height: 60%">
+
+                <q-card class="q-mx-lg bg-accent col-3 row" style="border-radius: 20px; position: relative;">
+                  <q-card-section class="col-5 row justify-center items-center">
+                    <div class="bg-secondary" style="position: absolute; height: 80%; width: 60%; border-radius: 10px;">
+                      <img style="height: 100%; width: 100%; border-radius: 10px;"
+                        src="https://www.ringtina.com.ar/Descargar/Fondos%20de%20Pantalla/Arquitectura/ImgArquitec%20032.jpg">
+                    </div>
+                  </q-card-section>
+                  <q-card-section class="col-6 row items-center no-marging no-padding">
+                    <div style="height: 80%; width: 100%;">
+                      <p class="col-12 text-bold" style="font-size: 18px; text-overflow: ellipsis;">Castellum</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Jeslar</p>
+                    </div>
+                  </q-card-section>
+                  <div style="height: 15px; width: 15px; border-radius: 10px; position: absolute; right: 22%; top: 60%;">
+                    <q-btn flat to="/" size="15px" icon="mdi-arrow-right-bold-circle-outline"
+                      :color="$route.name === 'dashboard' ? 'primary' : 'primary'" />
+                  </div>
+                </q-card>
+
+                <q-card class="q-mx-lg bg-accent col-3 row" style="border-radius: 20px;">
+                  <q-card-section class="col-5 row justify-center items-center">
+                    <div class="bg-secondary" style="position: absolute; height: 80%; width: 60%; border-radius: 10px;">
+                      <img style="height: 100%; width: 100%; border-radius: 10px;"
+                        src="https://www.ringtina.com.ar/Descargar/Fondos%20de%20Pantalla/Arquitectura/ImgArquitec%20032.jpg">
+                    </div>
+                  </q-card-section>
+                  <q-card-section class="col-6 row items-center no-marging no-padding">
+                    <div style="height: 80%; width: 100%;">
+                      <p class="col-12 text-bold" style="font-size: 18px; text-overflow: ellipsis;">Castellum</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Jeslar</p>
+                    </div>
+                  </q-card-section>
+                  <div style="height: 15px; width: 15px; border-radius: 10px; position: absolute; right: 22%; top: 60%;">
+                    <q-btn flat to="/" size="15px" icon="mdi-arrow-right-bold-circle-outline"
+                      :color="$route.name === 'dashboard' ? 'primary' : 'primary'" />
+                  </div>
+                </q-card>
+                <q-card class="q-mx-lg bg-accent col-3 row" style="border-radius: 20px;">
+                  <q-card-section class="col-5 row justify-center items-center">
+                    <div class="bg-secondary" style="position: absolute; height: 80%; width: 60%; border-radius: 10px;">
+                      <img style="height: 100%; width: 100%; border-radius: 10px;"
+                        src="https://www.ringtina.com.ar/Descargar/Fondos%20de%20Pantalla/Arquitectura/ImgArquitec%20032.jpg">
+                    </div>
+                  </q-card-section>
+                  <q-card-section class="col-6 row items-center no-marging no-padding">
+                    <div style="height: 80%; width: 100%;">
+                      <p class="col-12 text-bold" style="font-size: 18px; text-overflow: ellipsis;">Castellum</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Jeslar</p>
+                    </div>
+                  </q-card-section>
+                  <div style="height: 15px; width: 15px; border-radius: 10px; position: absolute; right: 22%; top: 60%;">
+                    <q-btn flat to="/" size="15px" icon="mdi-arrow-right-bold-circle-outline"
+                      :color="$route.name === 'dashboard' ? 'primary' : 'primary'" />
+                  </div>
+                </q-card>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 row justify-center items-start" style="height: 38%;">
+          <div class="col-11 row">
+            <div class="col-12 row justify-start items-center ">
+              <p class="col-10 q-mb-md text-weight-medium text-h4 no-marging">Personajes</p>
+            </div>
+            <div class="col-12 bg-primary row justify-center items-center" style="height: 150px; border-radius: 38px;">
+              <div class="col-12 row" :class="array == 2 ? 'justify-start' : 'justify-between'" style="height: 60%">
+
+                <q-card class="q-mx-lg bg-accent col-3 row" style="border-radius: 20px;">
+                  <q-card-section class="col-5 row justify-center items-center">
+                    <div class="bg-secondary" style="position: absolute; height: 80%; width: 60%; border-radius: 10px;">
+                      <img style="height: 100%; width: 100%; border-radius: 10px;"
+                        src="https://i.pinimg.com/736x/c0/24/df/c024dfaaada11a57aadb6adf107f4dbc.jpg">
+                    </div>
+                  </q-card-section>
+                  <q-card-section class="col-6 row items-center no-marging no-padding">
+                    <div style="height: 80%; width: 100%;">
+                      <p class="col-12 text-bold" style="font-size: 14px; text-overflow: ellipsis;">Annabeth Blathorne</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Princesa</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Bellatorum</p>
+                    </div>
+                  </q-card-section>
+                  <div style="height: 15px; width: 15px; border-radius: 10px; position: absolute; right: 22%; top: 60%;">
+                    <q-btn flat to="/" size="15px" icon="mdi-arrow-right-bold-circle-outline"
+                      :color="$route.name === 'dashboard' ? 'primary' : 'primary'" />
+                  </div>
+                </q-card>
+
+                <q-card class="q-mx-lg bg-accent col-3 row" style="border-radius: 20px;">
+                  <q-card-section class="col-5 row justify-center items-center">
+                    <div class="bg-secondary" style="position: absolute; height: 80%; width: 60%; border-radius: 10px;">
+                      <img style="height: 100%; width: 100%; border-radius: 10px;"
+                        src="https://i.pinimg.com/736x/c0/24/df/c024dfaaada11a57aadb6adf107f4dbc.jpg">
+                    </div>
+                  </q-card-section>
+                  <q-card-section class="col-6 row items-center no-marging no-padding">
+                    <div style="height: 80%; width: 100%;">
+                      <p class="col-12 text-bold" style="font-size: 14px; text-overflow: ellipsis;">Annabeth Blathorne</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Princesa</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Bellatorum</p>
+                    </div>
+                  </q-card-section>
+                  <div style="height: 15px; width: 15px; border-radius: 10px; position: absolute; right: 22%; top: 60%;">
+                    <q-btn flat to="/" size="15px" icon="mdi-arrow-right-bold-circle-outline"
+                      :color="$route.name === 'dashboard' ? 'primary' : 'primary'" />
+                  </div>
+                </q-card>
+                <q-card class="q-mx-lg bg-accent col-3 row" style="border-radius: 20px;">
+                  <q-card-section class="col-5 row justify-center items-center">
+                    <div class="bg-secondary" style="position: absolute; height: 80%; width: 60%; border-radius: 10px;">
+                      <img style="height: 100%; width: 100%; border-radius: 10px;"
+                        src="https://i.pinimg.com/736x/c0/24/df/c024dfaaada11a57aadb6adf107f4dbc.jpg">
+                    </div>
+                  </q-card-section>
+                  <q-card-section class="col-6 row items-center no-marging no-padding">
+                    <div style="height: 80%; width: 100%;">
+                      <p class="col-12 text-bold" style="font-size: 14px; text-overflow: ellipsis;">Annabeth Blathorne</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Princesa</p>
+                      <p class="col-12 text-weight-light" style="font-size: 11px;">Bellatorum</p>
+                    </div>
+                  </q-card-section>
+                  <div style="height: 15px; width: 15px; border-radius: 10px; position: absolute; right: 22%; top: 60%;">
+                    <q-btn flat to="/" size="15px" icon="mdi-arrow-right-bold-circle-outline"
+                      :color="$route.name === 'dashboard' ? 'primary' : 'primary'" />
+                  </div>
+                </q-card>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class=" full-height row justify-center items-center col-3">
+      <div class="col-10 bg-accent" style="height: 85%; border-radius: 66px;">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue'
-import { getData } from '../composables/useQuery'
+// import { getData } from '../composables/useQuery'
 
 export default {
   name: 'HomePage',
   setup() {
-    const { query, login, nickName, password } = getData()
+    // const { query, login, nickName, password } = getData()
     const leftDrawerOpen = ref(false)
+    const userName = ref("MysteriousAngel")
+    const array = ref(3)
     onMounted(() => {
-      query()
+      // query()
     })
     return {
       leftDrawerOpen,
-      nickName,
-      password,
-      login,
+      userName,
+      array,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
@@ -31,62 +195,9 @@ export default {
 </script>
 
 <style >
-@media screen and (max-width: 1024px) {
-  .form-login {
-    margin: 0px auto;
-  }
-}
-
-.form-login {
-  background: linear-gradient(to bottom, #8D094D, #1441B5);
-  border-radius: 20px;
-  box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.8);
-}
-
-.right-side {
-  background: transparent;
-  text-shadow: 8px 8px 10px rgba(205, 205, 205, 0.5);
-  ;
-}
-
-.logo {
-  border-radius: 50%;
-  width: 90px;
-  height: 90px;
-}
-
-.logo2 {
-  border-radius: 50%;
-  width: 170px;
-  height: 170px;
-  /* margin-top: 30px;*/
-  margin-left: 50px;
-}
-
-.right-text {
-  opacity: 0.8;
-  max-width: 400px;
-  text-align: center;
-  margin-top: -100px;
-  line-height: 1.0;
-}
-
-.inputLogin {
-  height: 20px;
-}
-
-.q-field--outlined.q-field--rounded .q-field__control {
-  border-radius: 28px;
-  height: 45px;
-}
-
-hr {
-  height: 1px;
-  width: 100%;
-  background-color: #E0E0E0;
-}
-
-.section {
-  width: 100%;
+p {
+  margin: 0 0 0px;
+  white-space: nowrap;
+  overflow: hidden;
 }
 </style>
