e4daaf91042b04f8dcb53ce7e1c43b1c6146d0c1
CarbrielysBasantaG <orinocoDevTeDe6@gmail.com>
2023-06-15 20:35:52 -0400

    Listo para la conexión

--- src/pages/HomeMenuPage.vue
+++ src/pages/HomeMenuPage.vue
@@ -3,6 +3,7 @@
   <div class=" row col-12" style=" height: 100vh;">
     <div class="row justify-end items-center col-9">
       <div class="col-10 bg-accent q-mr-xl row justify-center" style="height: 85%; border-radius: 0px 100px 0px 100px;">
+
         <div class="col-12 row justify-between" style="height: 24%;">
           <div class="bg-white q-ml-lg q-mt-lg col-7 row justify-center items-center content-center"
             style="border-radius: 0px 60px 0px 60px; height: 60%;">
@@ -17,6 +18,7 @@
             <q-img src="../img/Sapphire.png" class="q-my-xs" style="height: 100px; width: 100%" />
           </div>
         </div>
+
         <div class="col-12 row justify-center items-start" style="height: 38%;">
           <div class="col-11 row">
             <div class="col-12 row justify-start items-center ">
@@ -160,9 +162,31 @@
         </div>
       </div>
     </div>
+
     <div class=" full-height row justify-center items-center col-3">
       <div class="col-10 bg-accent" style="height: 85%; border-radius: 66px;">
-
+        <q-carousel infinite :autoplay="autoplay" @mouseenter="autoplay = false" @mouseleave="autoplay = true"
+          transition-prev="scale" transition-next="scale" v-model="slide" swipeable animated :control-type="controlType"
+          control-color="secondary" navigation padding arrows height="90%" class="text-secondary bg-transparent ">
+          <q-carousel-slide name="1" class="column no-wrap flex-start">
+            <q-icon color="primary" name="mdi-book-open-page-variant" size="86px" class="col-5" />
+            <div class="q-mt-md text-center text-h6 text-weight-light">
+              Da rienda suelta a tu imaginación al escribir sin perder la coherencia en los detalles.
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="2" class="column no-wrap flex-start">
+            <q-icon color="secondary" name="mdi-earth-plus" size="86px" class="col-5" />
+            <div class="q-mt-md text-center text-h6 text-weight-light">
+              Adéntrate en tu mundo definiendo cada detalle.
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="3" class="column no-wrap flex-start">
+            <q-icon color="indigo-14" name="mdi-account-multiple-plus" size="86px" class="col-5" />
+            <div class="q-mt-md text-center text-h6 text-weight-light">
+              Sapphire te ayudará a organizar la personalidad de tus personajes.
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
       </div>
     </div>
   </div>
@@ -188,7 +212,11 @@ export default {
       array,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      },
+      controlType: ref('flat'),
+      autoplay: ref(true),
+      slide: ref('1'),
+      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
     }
   }
 }
